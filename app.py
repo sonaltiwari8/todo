84e5b611ad6d4abe114acb91046d1b51588f7208
rohan soni <rohansoni54476@gmail.com>
2024-08-23 08:45:35 +0530

    add create todo endpoint

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from controllers.todo_controller import render_main_page
+from controllers.todo_controller import create_todo_handler, render_main_page
 from flask import Flask
 
+from models.todo import Todo
+
+# app -> controller -> models -> database
 
 app = Flask(__name__)
 
@@ -13,6 +16,13 @@ app = Flask(__name__)
 @app.get("/")
 def render_main():
     return render_main_page()
+
+# flask will call the create_todo function automatically whenever there is POST request on the /todos endpoint
+
+
+@app.post("/todos")
+def create_todo():
+    return create_todo_handler()
 
 
 app.run('localhost', 4000, debug=True)
